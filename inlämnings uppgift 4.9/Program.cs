7cfff1a2a2e91806a29b0802fe949bc277097466
dean06001 <dean06001@E-PW0234KC>
2023-10-19 15:28:48 +0200

    Add project files.

---
+++ inlämnings uppgift 4.9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift_4._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programmet ritar ut en rätvinklig triangel.");
+            Console.WriteLine("Bestäm sidlängden på triangeln.");
+            int längd = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < längd; i++)
+            {
+                string triangle = new string('*', i + 1);
+                Console.WriteLine(triangle);
+            }
+        }
+    }
+}
